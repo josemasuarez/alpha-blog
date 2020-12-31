3e650674f4c1d3dca0d5782f6c163ef8021f7450
Jose Maria Suarez <jossuarez@paypal.com>
2020-12-31 11:51:30 -0300

    Add edit methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-
+    validates :title, presence: true, length: { minimun: 6, maximum: 20 } 
+    validates :description, presence: true, length: { minimun: 10, maximum: 300 } 
 end
